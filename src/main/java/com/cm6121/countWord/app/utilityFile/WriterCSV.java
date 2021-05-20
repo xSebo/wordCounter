ae7b4147c2a0b41105a978e461ed94347d30120c
Seb <barnards3@cardiff.ac.uk>
2021-05-20 14:08:17 +0100

    Further testing commit messages yet again

--- src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
+++ src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class WriterCSV {
     private final String saveDir = System.getProperty("user.home")+"\\StudentCSVSaved\\";
-    // Test comment 2
+    // Test comment 3
     private void createDirectory(){
         File dir = new File(saveDir);
         if(!dir.exists()){
