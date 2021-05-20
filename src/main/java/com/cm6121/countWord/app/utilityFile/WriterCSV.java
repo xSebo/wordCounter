06af1826d559df2e2ecfc38adee730d32aef8fc0
Seb <barnards3@cardiff.ac.uk>
2021-05-20 14:13:41 +0100

    Test

--- src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
+++ src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class WriterCSV {
     private final String saveDir = System.getProperty("user.home")+"\\StudentCSVSaved\\";
-    // Test comment 3
+    // Test comment 4
     private void createDirectory(){
         File dir = new File(saveDir);
         if(!dir.exists()){
