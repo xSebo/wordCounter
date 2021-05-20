fe2c996774c982d2d69d7ffd674d4bdf551e662d
xSebo <23667880+xSebo@users.noreply.github.com>
2021-05-20 14:34:46 +0100

    Corrected merge error

--- src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
+++ src/main/java/com/cm6121/countWord/app/utilityFile/WriterCSV.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class WriterCSV {
     private final String saveDir = System.getProperty("user.home")+"\\StudentCSVSaved\\";
-    // Test comment 6
+    // Test comment 7
     private void createDirectory(){
         File dir = new File(saveDir);
         if(!dir.exists()){
